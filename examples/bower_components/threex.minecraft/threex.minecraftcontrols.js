fc9f8bd02eb91cb335823f3b119d5b7a4743bf2c
Jerome Etienne <jerome.etienne@gmail.com>
2014-05-03 11:34:04 +0200

    more wokr

---
+++ examples/bower_components/threex.minecraft/threex.minecraftcontrols.js
@@ -0,0 +1,41 @@
+var THREEx	= THREEx || {};
+
+THREEx.MinecraftControls	= function(object3d, input){
+	// arguments default values
+	input		= input	|| {}
+
+	// handle arguments default values
+	this.speed	= 2;
+	this.input	= input;
+	this.object3d	= object3d;
+	
+	// user control
+	this.update	= function(delta, now){
+		var prevPosition	= object3d.position.clone();
+		// rotation
+		if( input.left )	object3d.rotation.y += 0.1 * delta * Math.PI * 2;
+		if( input.right )	object3d.rotation.y -= 0.1 * delta * Math.PI * 2;			
+
+		// strafe
+		var distance	= 0;
+		if( input.strafeLeft )	distance	= +this.speed * delta;
+		if( input.strafeRight )	distance	= -this.speed * delta;
+		if( distance ){
+			var velocity	= new THREE.Vector3(distance, 0, 0);
+			var matrix	= new THREE.Matrix4().makeRotationY(object3d.rotation.y);
+			velocity.applyMatrix4( matrix );
+			object3d.position.add(velocity);
+		}
+
+		// up/down
+		var distance	= 0;
+		if( input.up )		distance	= +this.speed * delta;
+		if( input.down )	distance	= -this.speed * delta;
+		if( distance ){
+			var velocity	= new THREE.Vector3(0, 0, distance);
+			var matrix	= new THREE.Matrix4().makeRotationY(object3d.rotation.y);
+			velocity.applyMatrix4( matrix );
+			object3d.position.add(velocity);
+		}
+	}
+}
